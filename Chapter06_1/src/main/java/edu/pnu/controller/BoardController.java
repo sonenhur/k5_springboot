8cf9556fdcb9984e1f8e9262cf6148d4d1adcc84
sonenhur <148170713+sonenhur@users.noreply.github.com>
2024-01-19 12:05:48 +0900

    Add files via upload

--- Chapter06_1/src/main/java/edu/pnu/controller/BoardController.java
+++ Chapter06_1/src/main/java/edu/pnu/controller/BoardController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.pnu.service.BoardService;
@@ -14,12 +13,16 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 	
-	@RequestMapping("/getBoardList")
+	@GetMapping("/getBoardList")
 	public String getBoardList(Model model) {
 		model.addAttribute("boardList", boardService.getBoardList());
 		return "getBoardList";
 	}
-	
+	@GetMapping("/getBoard")
+	public String getBoard(Long seq, Model model) {
+		model.addAttribute("board", boardService.getBoard(seq));
+		return "getBoard";
+	}
 	@GetMapping("/hello")
 	public void hello(Model model) {
 		model.addAttribute("greeting", "Hello 타임리프. ^^");
--- Chapter06_1/src/main/java/edu/pnu/service/BoardService.java
+++ Chapter06_1/src/main/java/edu/pnu/service/BoardService.java
@@ -1,8 +1,8 @@
 package edu.pnu.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import edu.pnu.domain.Board;
@@ -11,19 +11,15 @@ import edu.pnu.persistence.BoardRepository;
 @Service
 public class BoardService {
 
-    public BoardService(BoardRepository boardRepository) {
-    }
-
-	public Object getBoardList() {
-		List<Board> boardList = new ArrayList<>();
-		for (long i=1L; i<=10L; i++) {
-			boardList.add(Board.builder()
-					.seq(i)
-					.title("게시판 프로그램 테스트")
-					.writer("도우너")
-					.content("게시판 프로그램 테스트입니다...")
-					.build());
-		}
-		return boardList;
+	@Autowired
+	private BoardRepository boardRepository;
+	
+	public List<Board> getBoardList() {
+		return boardRepository.findAll();
 	}
+
+	public Board getBoard(Long seq) {
+		return boardRepository.findById(seq).get();
+	}
+
 }
