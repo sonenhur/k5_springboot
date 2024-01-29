4b26ce4b8c514839d0fc3def490699fd4cb5bcc9
sonenhur <148170713+sonenhur@users.noreply.github.com>
2024-01-29 17:39:49 +0900

    Delete SecurityJWT review1 directory

--- SecurityJWT review1/src/main/java/edu/pnu/domain/Role.java
+++
@@ -1,5 +0,0 @@
-package edu.pnu.domain;
-
-public enum Role {
-    ROLE_ADMIN, ROLE_MANAGER, ROLE_MEMBER
-}
--- SecurityJWT review1/src/main/java/edu/pnu/persistence/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.pnu.persistence;
-
-import edu.pnu.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
